b9e721e14a92d850044cbcc6539fa790e8df74e0
Emmanuel Schweitzer <eschweit@tibco.com>
2018-04-11 23:12:40 +1000

    Update wipe.sh

--- tibco.bwce.sample.binding.rest.BookStore.application.parent/wipe.sh
+++ tibco.bwce.sample.binding.rest.BookStore.application.parent/wipe.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-EXPORT PGPASSWORD=$1
+export PGPASSWORD="$1"
 echo "DELETE FROM books" | psql --host ecs-bookstore-db.cptwyoo6cojq.ap-southeast-2.rds.amazonaws.com --user tibco --dbname BookstoreDB_Test
